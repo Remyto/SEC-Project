29e6469e67cc613dd25a8608cc426ad885a0705b
Remyto <remycherkaoui@gmail.com>
2022-03-25 19:00:59 +0100

    PostCreate.js

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+/// Get a list of all the posts that have ever been created
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  /// Store list of post
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
+    /// Request to the posts service
     const res = await axios.get("http://localhost:4000/posts");
 
     setPosts(res.data);
@@ -13,9 +17,28 @@ export default () => {
 
   useEffect(() => {
     fetchPosts();
-  }, []); // Empty arrray so that useEffect onlt run the function once
+  }, []); // Empty arrray so that useEffect only runs the function once
 
   console.log(posts);
 
-  return <div></div>;
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+            {/* Should be {post.title} */}
+          <h3>{post.title.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex felx-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 };
